eec556421ebe16c44b9d59c502b81f825f6b0ddc
Sébastien <sebastien@svallee.fr>
2018-02-28 11:09:19 +0100

    New year !

--- hms_agenda/strings.py
+++ hms_agenda/strings.py
@@ -1,7 +1,7 @@
 
 
 SEANCE_MESSAGES = (
-    "Si pour 2017 t'as des projets, viens nous voir et nous en parler !",
+    "Si pour 2018 t'as des projets, viens nous voir et nous en parler !",
     "Si tu aimes bricoler, viens t'amuser avec nous !",
     "Tant que tu n'as pas essayé tu peux encore te passer de nous... VIENS !",
     "Si hacker est pour toi plus qu'un truc qu'on entend aux infos, passe nous voir !",
